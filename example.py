d28d7587a7ce7d5056db4d2fc31544d5525125a4
yanxiao0201 <yanxiao0201@gmail.com>
2017-05-27 11:13:37 -0700

    xiao

--- example.py
+++ example.py
@@ -1,7 +1,14 @@
 from data_reader import *
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+
+
 clf=DecisionTreeClassifier()
 my_reader=data_reader()
 X,Y=my_reader.read_array()
+
+
+
+test_data = data_reader()
 clf.fit(X,Y)
 my_reader.save_data()
