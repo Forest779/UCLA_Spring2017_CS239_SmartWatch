50c343c28265b50fa0f810e899d1927905ae0a8e
HaoLovesCoding <nanocmp@gmail.com>
2017-05-27 11:10:54 -0700

    generic path

--- example.py
+++ example.py
@@ -2,6 +2,6 @@ from data_reader import *
 from sklearn.tree import DecisionTreeClassifier
 clf=DecisionTreeClassifier()
 my_reader=data_reader()
-X,Y=my_reader.read_array()
+X,Y=my_reader.read_array('/Users/HaoWu/Documents/Code/CS239/Data Set')
 clf.fit(X,Y)
-my_reader.save_data()
+my_reader.save_data('test')
