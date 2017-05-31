3e68658056aeda794078cd2b563e2f427e3a59e4
yanxiao0201 <yanxiao0201@gmail.com>
2017-05-31 10:10:59 -0700

    xiao

--- example.py
+++ example.py
@@ -5,21 +5,21 @@ from sklearn.metrics import *
 
 # Data Processing
 Tran_reader = data_reader()
-X_tran,Y_tran = Tran_reader.read_array('/Users/HaoWu/Documents/Code/CS239/Data Set 2/Dashing Gu') # Training set
+X_tran,Y_tran = Tran_reader.read_array('/Users/xiaoyan/Google Drive/CS-239/Data Set/Zhehan Li') # Training set
 Test_reader = data_reader()
-X_test,Y_true = Test_reader.read_array('/Users/HaoWu/Documents/Code/CS239/Data Set 2/Zhehan Li')    # Test set
+X_test,Y_true = Test_reader.read_array('/Users/xiaoyan/Google Drive/CS-239/Data Set/Xiao Yan')    # Test set
 
 # Model Training
 clf = RandomForestClassifier(n_estimators=500, max_depth = 100)
-model = OneVsOneClassifier(clf, n_jobs=-1)
+model = OneVsRestClassifier(clf, n_jobs=-1)
 model.fit(X_tran,Y_tran)
 y_test = model.predict(X_test)
 
 print 'reading done'
 # Results:
-acc = accuracy_score(Y_true[:,0], y_test[:,0], normalize=True, sample_weight=None)
+acc = accuracy_score(Y_true, y_test, normalize=True, sample_weight=None)
 print "Accuracy: ",acc
-recall = recall_score(Y_true[:,0], y_test[:,0], average=None)
-print "Recall: Sitting: ", recall[0], "Standing: ", recall[1], "Walking: ", recall[2]
-precision = precision_score(Y_true[:,0], y_test[:,0], average=None)
-print "Precision: Sitting: ", precision[0], "Standing: ", precision[1], "Walking: ", precision[2]
+#recall = recall_score(Y_true, y_test, average=None)
+#print "Recall: Sitting: ", recall[0], "Standing: ", recall[1], "Walking: ", recall[2]
+#precision = precision_score(Y_true, y_test, average=None)
+#print "Precision: Sitting: ", precision[0], "Standing: ", precision[1], "Walking: ", precision[2]
