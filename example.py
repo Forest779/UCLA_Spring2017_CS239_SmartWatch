d1e40c7644cff6d873a5e013d8120c9566f62152
yanxiao0201 <yanxiao0201@gmail.com>
2017-06-01 14:00:11 -0700

    xiao

--- example.py
+++ example.py
@@ -4,19 +4,34 @@ from sklearn.multiclass import OneVsRestClassifier
 
 from sklearn.metrics import *
 
+
+root = "/Users/xiaoyan/Google Drive/CS-239/Data Set/"
+namelist = ["Dashing Gu","Zhehan Li", "Xiao Yan"]
+
+test_name = "Test"
 # Data Processing
 Tran_reader = data_reader()
-X_tran,Y_tran = Tran_reader.read_array('/Users/xiaoyan/Google Drive/CS-239/Data Set/Zhehan Li') # Training set
+
+X_train_list = []
+Y_train_list = []
+for name in namelist:
+    X_tran,Y_tran = Tran_reader.read_array(root+name) # Training set
+    X_train_list.append(X_tran)
+    Y_train_list.append(Y_tran)
+
+X_train_total = np.concatenate(X_train_list,axis=0)
+Y_train_total = np.concatenate(Y_train_list,axis=0)
+Y_train_total = np.asarray(Y_train_total).reshape(len(Y_train_total),)
+print len(Y_train_total)
+
 Test_reader = data_reader()
-X_test,Y_true = Test_reader.read_array('/Users/xiaoyan/Google Drive/CS-239/Data Set/Dashing Gu')    # Test set
+X_test,Y_true = Test_reader.read_array(root+test_name)    # Test set
 
 # Model Training
-#clf = RandomForestClassifier(n_estimators=500, max_depth = 100)
-#model = OneVsRestClassifier(clf, n_jobs=-1)
-#model.fit(X_tran,Y_tran)
-#y_test = model.predict(X_test)
+clf = RandomForestClassifier(n_estimators=500, max_depth = 100)
 
-#SVM method
+clf.fit(X_train_total,Y_train_total)
+y_test = clf.predict(X_test)
 
 
 print 'reading done'
